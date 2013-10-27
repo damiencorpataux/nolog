a99ef5b7f40bfa44a29f7c6201f846a18e9bc3d8
Damien Corpataux <d@mien.ch>
2013-10-27 02:07:51 +0100

    filter/syslog to accept an arbitrary year (n.b. syslog flavour loglines do not provide a year

--- processor/filter/syslog.py
+++ processor/filter/syslog.py
@@ -8,14 +8,14 @@ from datetime import datetime
 # Oct 25 07:03:28 localhost nullmailer[2319]: Sending failed:  Host not found
 regex = re.compile('(?P<time>\w{3} \d{1,2} \d\d:\d\d:\d\d) (?P<host>.*?) (?P<rawprocess>(?P<process>.+?)(\[\d+\])?): (?P<message>.*)')
 
-def process(data):
+def process(data, year=None):
     for line in data:
         m = regex.search(line).groupdict()
         # FIXME: Timezone
         # TODO: Dates should be parsed in a factorized way
+        year = year if year else datetime.now().year
         m['timestamp'] = time.mktime(time.strptime(
-            # tweaking non-existing year with current year
-            '%s %s' % (datetime.now().year, m.get('time')),
+            '%s %s' % (year, m.get('time')), # add current year to logline
             '%Y %b %d %H:%M:%S'
         ))
         yield m
